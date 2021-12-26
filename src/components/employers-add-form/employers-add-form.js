842eab533d7d6826a5514bdb685eaa8170481d02
Abdurahmon <87411075+AbdurahmonQobiljonov@users.noreply.github.com>
2021-12-26 19:39:18 +0500

    change Uncontrolled as Controlle component

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -2,13 +2,36 @@ import {Component} from 'react';
 import './employers-add-form.css';
 
 class EmployeesAddForm extends Component{
+
+   state = {
+      name:'',
+      salary:''
+   }
+
+   onChangeInput=(e)=>{
+      this.setState({
+         [e.target.name]:e.target.value
+      })
+   }
+
   render() {
+      const {name,salary} = this.state
      return (
         <div className="app-add-form">
            <h3>Добавьте нового сотрудника</h3>
            <form className="add-form d-flex">
-              <input type="text" className="form-control new-post-label" placeholder="Как его зовут?" />
-              <input type="number" className="form-control new-post-label" placeholder="З/П в $?" />
+              <input type="text"
+                     onChange={this.onChangeInput}
+                     value={name} name='name'
+                     className="form-control new-post-label"
+                     placeholder="Как его зовут?"
+              />
+              <input type="number"
+                     onChange={this.onChangeInput}
+                     value={salary} name='salary'
+                     className="form-control new-post-label"
+                     placeholder="З/П в $?"
+              />
 
               <button type="submit" className="btn btn-outline-light">
                  Добавить
@@ -17,6 +40,6 @@ class EmployeesAddForm extends Component{
         </div>
      );
   }
-};
+}
 
 export default EmployeesAddForm;
